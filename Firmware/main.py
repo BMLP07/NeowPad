e33d3ad8408c13ebd33ee43128251dfa70535af3
BM <232473223+BMLP07@users.noreply.github.com>
2026-05-31 02:19:08 +0200

    Refactor KMK Keyboard setup with media keys support

--- Firmware/main.py
+++ Firmware/main.py
@@ -6,46 +6,49 @@ from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.modules.encoder import EncoderHandler
 from kmk.modules.macros import Macros
+from kmk.extensions.media_keys import MediaKeys
 
-# Initialize the keyboard and macros module
+# Initialize the keyboard, macros and encoder modules
 keyboard = KMKKeyboard()
 macros = Macros()
+encoder_handler = EncoderHandler()
+
 keyboard.modules.append(macros)
+keyboard.modules.append(encoder_handler)
+keyboard.extensions.append(MediaKeys())
 
 # Define the key matrix
-keyboard.row_pins = (board.GP4, board.GP3, board.GP2) 
-keyboard.col_pins = (board.GP26, board.GP27, board.GP28, board.GP29, board.GP6)
+keyboard.row_pins = (board.D8, board.D10, board.D9) 
+keyboard.col_pins = (board.D0, board.D1, board.D2, board.D3, board.D4)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-# Define rotary encoder
-encoder_handler = EncoderHandler()
-keyboard.modules.append(encoder_handler)
-
+# Define the rotary encoder
 encoder_handler.pins = (
-    (board.GP7, board.GP1, board.GP0, False),
+    (board.D5, board.D6, board.D7, True),
 )
 
 # Macros 
-MAIL_MACRO = KC.MACRO("hello@earth.com")
+MAIL_MACRO = KC.MACRO("esto funciona")
 
-LOCK_PC = KC.MACRO(KC.LCMD(KC.L))
-
-UNDO = KC.MACRO(KC.LCTL(KC.Z))
+LOCK_PC  = KC.LWIN(KC.L)
 
 # Keymap
 keyboard.keymap = [
     [
-        MAIL_MACRO, UNDO, LOCK_PC, KC.N9, KC.N0,
+        # First row D8
+        LOCK_PC, KC.NO, MAIL_MACRO, KC.NO, KC.N0,
         
-        KC.N1, KC.N2, KC.N3, KC.N4, KC.N5,
+        # Second row D10
+        KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, 
         
+        # Third row D9
         KC.A,  KC.B,  KC.C,  KC.D,  KC.E,
     ]
 ]
 
 # Encoder Actions
 encoder_handler.map = [
-    ((KC.VOLD, KC.VOLU, KC.MUTE),), # Vol Down / Vol Up / Mute
+    ((KC.VOLD, KC.VOLU, KC.MUTE),), 
 ]
 
 if __name__ == '__main__':
